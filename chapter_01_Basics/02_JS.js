183aa4ef65d65c36a00996f23f3b15307d4cb6ce
rajeshpatil235 <rajeshpatil235@gmail.com>
2026-05-06 22:36:29 +0530

    Initial commit: Add Playwright learning project files

---
+++ chapter_01_Basics/02_JS.js
@@ -0,0 +1,11 @@
+let a = 10
+console.log(a)
+
+for (let i = 0; i < 100000; i++) {
+    console.log(a)
+    print()
+}
+
+function print() {
+    console.log("Hello")
+}
---
+++ chapter_01_Basics/03_Commands.js
@@ -0,0 +1,10 @@
+console.log(process.platform)
+
+//mac - darwin
+//windows - win32
+//linux - linux
+
+console.log(process.arch)
+//x64
+
+console.log("Node version: ", process.version)
